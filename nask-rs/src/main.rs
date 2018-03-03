3196fbc8852caf8a3750bee299fd57a5adebff08
taba <hgKaguyaToupako4er@gmail.com>
2018-03-03 16:16:31 +0900

    Refactaring (don't work)

--- nask-rs/src/main.rs
+++ nask-rs/src/main.rs
@@ -1,6 +1,5 @@
 use std::io::{BufWriter, Write};
 use std::io::{Read, BufRead, BufReader, self};
-use std::str::FromStr;
 use std::collections::HashMap;
 
 extern crate nask_rs;
@@ -8,13 +7,26 @@ use nask_rs::parser::parse;
 use nask_rs::parser::Rule;
 
 extern crate pest;
-
 //#[derive(PartialEq, Debug)]
 //enum Absyn<'a> {
 //    DB(LinkedList<&'a str>),
 //    RESB(u32)
 //}
 //
+//
+
+// TODO to add a limit to num
+fn to_u8(imm: &str, rule: Rule) -> io::Result<u8> {
+    match rule {
+        Rule::hex8 => {
+            u8::from_str_radix(imm, 16).unwrap()
+        },
+        Rule::dec8 => {
+            u8::from_str_radix(imm, 10).unwrap()
+        },
+        _ => unreachable!() 
+    }
+}
 
 fn main() {
     let mut codes = vec![];
@@ -30,6 +42,7 @@ fn main() {
     reader.read_to_end(&mut rbuff).unwrap();
 
 
+
     for line1 in rbuff.lines() {
         let line = line1.unwrap();
         let mut pairs = parse(&line);
@@ -40,29 +53,27 @@ fn main() {
             Rule::empty => {
             },
             Rule::org => {
-                let operand = pairs.next().unwrap().into_span().as_str();
-                current_address += u16::from_str_radix(operand, 16).unwrap();
+                let op = pairs.next().unwrap().as_str();
+                //current_address += u16::from_str_radix(op, 16).unwrap();
             },
             Rule::db => {
-                for pair in pairs {
-                    let operand = pair.clone().into_span().as_str();
-                    match pair.as_rule() {
-                        Rule::hex2 => {
-                            let converted = u8::from_str_radix(operand, 16).unwrap();
-                            codes.push(converted);
-                         //   writer.write_fmt(format_args!(r"\x{:02X}", converted)).unwrap();
-                            current_address += 1;
-                        },
-                        Rule::num => {
-                            let converted = u8::from_str(operand).unwrap();
-                            codes.push(converted);
-                          //  writer.write_fmt(format_args!(r"\x{:02X}", converted)).unwrap();
+                for op in pairs.next() {
+                    match op.as_rule() {
+                        Rule::imm8 => {
+                            op.into_inner().next().unwrap();
+                            match op.into_inner().next() {
+                                Some(inner) => {
+                                    let a = inner.as_str();
+                                    let b = inner.as_rule();
+                                    codes.push(to_u8(a, b));
+                                },
+                                _ => unreachable!() 
+                            }
                             current_address += 1;
                         },
                         Rule::str => {
-                            for b in operand.bytes() {
+                            for b in op.as_str().bytes() {
                                 codes.push(b);
-                          //     writer.write_fmt(format_args!(r"\x{:02X}", b)).unwrap();
                                 current_address += 1;
                             }
                         },
@@ -71,98 +82,100 @@ fn main() {
                 }
             },
             Rule::dw => {
-                for pair in pairs {
-                    let operand = pair.clone().into_span().as_str();
-                    let b = {
-                        match pair.as_rule() {
-                            Rule::hex4 => {
-                                u16::from_str_radix(operand, 16).unwrap()
-                            },
-                            Rule::num => {
-                                u16::from_str_radix(operand, 10).unwrap()
-                            },
-                            _ => unreachable!()
+                let op1 = pairs.next().unwrap();
+                let mut b = 0x0000;
+                match op1.as_rule() {
+                    Rule::imm16 => {
+                        for inner in op1.into_inner() {
+                            match inner.as_rule() {
+                                Rule::hex16 => {
+                                    b = u16::from_str_radix(inner.as_str(), 16).unwrap()
+                                },
+                                Rule::dec16 => {
+                                    b = u16::from_str_radix(inner.as_str(), 10).unwrap()
+                                },
+                                _ => unreachable!() 
+                            }
                         }
-                    };
-                    let b0 =  b & 0x000F;
-                    let b1 = b & 0x00F0;//) >> 4;
-                    let b2 = (b & 0x0F00) >> 8;
-                    let b3 = (b & 0xF000) >> 8;
-                    codes.push((b1 + b0) as u8);
-                    codes.push((b3 + b2) as u8);
-                    //writer.write_fmt(format_args!(r"\x{:01X}{:01X}", b1, b0)).unwrap();
-                    //writer.write_fmt(format_args!(r"\x{:01X}{:01X}", b3, b2)).unwrap();
-                    current_address += 2;
+                    }
+                    _ => unreachable!()
                 }
+                let b10 =  b & 0x00FF;
+                let b32 = (b & 0xFF00) >> 8;
+                codes.push(b10 as u8);
+                codes.push(b32 as u8);
+                current_address += 2;
             },
             Rule::dd => {
-                for pair in pairs {
-                    let operand = pair.clone().into_span().as_str();
-                    let b = {
-                        match pair.as_rule() {
-                            Rule::hex8 => {
-                                u32::from_str_radix(operand, 16).unwrap()
-                            },
-                            Rule::num => {
-                                u32::from_str_radix(operand, 10).unwrap()
-                            },
-                            _ => unreachable!()
+                let op1 = pairs.next().unwrap();
+                let mut b = 0x0000;
+                match op1.as_rule() {
+                    Rule::imm32 => {
+                        for inner in op1.into_inner() {
+                            match inner.as_rule() {
+                                Rule::hex32 => {
+                                    b = u32::from_str_radix(inner.as_str(), 16).unwrap()
+                                },
+                                Rule::dec32 => {
+                                    b = u32::from_str_radix(inner.as_str(), 10).unwrap()
+                                },
+                                _ => unreachable!() 
+                            }
                         }
-                    };
-                    let b0 =  b & 0x0000000F;
-                    let b1 = (b & 0x000000F0);//) >>  4;
-                    let b2 = (b & 0x00000F00) >>  8;
-                    let b3 = (b & 0x0000F000) >>  8;
-                    let b4 = (b & 0x000F0000) >> 16;
-                    let b5 = (b & 0x00F00000) >> 16;
-                    let b6 = (b & 0x0F000000) >> 24;
-                    let b7 = (b & 0xF0000000) >> 24;
-                    codes.push((b1 + b0) as u8);
-                    codes.push((b3 + b2) as u8);
-                    codes.push((b5 + b4) as u8);
-                    codes.push((b7 + b6) as u8);
-                    //writer.write_fmt(format_args!(r"\x{:01X}{:01X}", b1, b0)).unwrap();
-                    //writer.write_fmt(format_args!(r"\x{:01X}{:01X}", b3, b2)).unwrap();
-                    //writer.write_fmt(format_args!(r"\x{:01X}{:01X}", b5, b4)).unwrap();
-                    //writer.write_fmt(format_args!(r"\x{:01X}{:01X}", b7, b6)).unwrap();
-                    current_address += 4;
+                    }
+                    _ => unreachable!()
                 }
+
+                let b10 =  b & 0x000000FF;
+                let b32 = (b & 0x0000FF00) >>  8;
+                let b54 = (b & 0x00FF0000) >> 16;
+                let b76 = (b & 0xFF000000) >> 24;
+                codes.push(b10 as u8);
+                codes.push(b32 as u8);
+                codes.push(b54 as u8);
+                codes.push(b76 as u8);
+                current_address += 4;
             },
             Rule::resb => {
-                let operand = pairs.next().unwrap();
-                match operand.as_rule() {
-                    Rule::hex4 => {
-                        let b = u16::from_str_radix(operand.into_span().as_str(), 16).unwrap();
-                        for _ in 0..(b - current_address) {
-                            codes.push(0);
-                    //        writer.write_fmt(format_args!(r"\x00")).unwrap();
-                            current_address += 1;
+                let op = pairs.next().unwrap();
+                match op.as_rule() {
+                    Rule::imm16 => {
+                        for inner in op.into_inner() {
+                            match inner.as_rule() {
+                                Rule::hex16 => {
+                                    let b = u16::from_str_radix(inner.as_str(), 16).unwrap();
+                                    for _ in 0..(b - current_address) {
+                                        codes.push(0);
+                                        current_address += 1;
+                                    }
+                                },
+                                Rule::dec16 => {
+                                    let n = u16::from_str_radix(inner.as_str(), 10).unwrap();
+                                    for _ in 0..n {
+                                        codes.push(0);
+                                        current_address += 1;
+                                    }
+                                },
+                                _ => unreachable!() 
+                            }
                         }
-                    },
-                    Rule::num  => {
-                        for _ in 0..u32::from_str(operand.into_span().as_str()).unwrap() {
-                            codes.push(0);
-                     //       writer.write_fmt(format_args!(r"\x00")).unwrap();
-                            current_address += 1;
-                        }
-
-                    },
+                    }
                     _ => unreachable!()
                 }
             },
             Rule::int => {
-                let operand = pairs.next().unwrap();
-                match operand.as_rule() {
-                    Rule::hex2 => {
-                        let s = operand.clone().into_span().as_str();
-                        let converted = u8::from_str_radix(s, 16).unwrap();
-                      //  writer.write_fmt(format_args!(r"\xCD\x{}", s)).unwrap();
-                        codes.push(205);
-                        codes.push(converted);
-                        current_address += 2;
-                    },
-                    _ => unreachable!()
-                }
+               // let operand = pairs.next().unwrap();
+               // match operand.as_rule() {
+                  //  Rule::hex2 => {
+                  //      let s = operand.clone().into_span().as_str();
+                  //      let converted = u8::from_str_radix(s, 16).unwrap();
+                  //    //  writer.write_fmt(format_args!(r"\xCD\x{}", s)).unwrap();
+                  //      codes.push(205);
+                  //      codes.push(converted);
+                  //      current_address += 2;
+                  //  },
+            //    _ => unreachable!()
+               // }
             },
             Rule::jmp => {
                 let label = pairs.next().unwrap().clone().into_span().as_str();
@@ -184,34 +197,41 @@ fn main() {
                 current_address += 1;
             },
             Rule::cmp => {
-                match pairs.next().unwrap().as_rule() {
-                    Rule::al => {
-                        codes.push(0x3C); 
-                        let operand = pairs.next().unwrap().clone().into_span().as_str();
-                        let converted = u8::from_str_radix(operand, 10).unwrap();
-                        codes.push(converted);
-                        current_address += 2;
-                    },
-                    _ => unreachable!()
-                }
+//                let op1 = pairs.next().unwrap();
+//                let op2 = pairs.next().unwrap();
+//
+//                match pairs.next().unwrap().as_rule() {
+//                    Rule::al => {
+//                        codes.push(0x3C); 
+//                        let operand = pairs.next().unwrap().clone().into_span().as_str();
+//                        let converted = u8::from_str_radix(operand, 10).unwrap();
+//                        codes.push(converted);
+//                        current_address += 2;
+//                    },
+//                    _ => unreachable!()
+//                }
             },
             Rule::add => {
-                match pairs.next().unwrap().as_rule() {
-                    Rule::si => {
-                            codes.push(0x83); 
-                            codes.push(0xC6);
-                            let operand = pairs.next().unwrap().clone().into_span().as_str();
-                            let converted = u8::from_str_radix(operand, 10).unwrap();
-                            codes.push(converted);
-                            current_address += 3;
-                    },
-                    _ => unreachable!()
-                }
+//                let op1 = pairs.next().unwrap();
+//                let op2 = pairs.next().unwrap();
+//
+//                match op1.as_rule() {
+//                    Rule::si => {
+//                            codes.push(0x83); 
+//                            codes.push(0xC6);
+//                            let converted = u8::from_str_radix(op2.as_str(), 10).unwrap();
+//                            codes.push(converted);
+//                            current_address += 3;
+//                    },
+//                    _ => unreachable!()
+//                }
             },
             Rule::mov => {
-                match pairs.next().unwrap().as_rule() {
+                let op1 = pairs.next().unwrap();
+                let op2 = pairs.next().unwrap();
+                match op1.as_rule() {
                     Rule::ss => {
-                        match pairs.next().unwrap().as_rule() {
+                        match op2.as_rule() {
                             Rule::ax => {
                                 codes.push(0x8E); 
                                 codes.push(0xD0);
@@ -222,7 +242,7 @@ fn main() {
                         }
                     },
                     Rule::ds => {
-                        match pairs.next().unwrap().as_rule() {
+                        match op2.as_rule() {
                             Rule::ax => {
                                 codes.push(0x8E); 
                                 codes.push(0xD8);
@@ -232,7 +252,7 @@ fn main() {
                         }
                     },
                     Rule::es => {
-                        match pairs.next().unwrap().as_rule() {
+                        match op2.as_rule() {
                             Rule::ax => {
                                 codes.push(0x8E); 
                                 codes.push(0xC0);
@@ -243,9 +263,24 @@ fn main() {
                         }
                     },
                     Rule::sp => {
+                        let mut b = 0x0000;
+                        match op2.as_rule() {
+                            Rule::imm16 => {
+                                let inner = op2.into_inner().next().unwrap();
+                                match inner.as_rule() {
+                                    Rule::hex16 => {
+                                        b = u16::from_str_radix(inner.as_str(), 16).unwrap();
+
+                                    },
+                                    Rule::dec16 => {
+                                        b = u16::from_str_radix(inner.as_str(), 10).unwrap();
+                                    }
+                                    _ => unreachable!()
+                                }
+                            },
+                            _ => unreachable!()
+                        }
                         codes.push(0xB8 + 0x04); 
-                        let operand = pairs.next().unwrap().clone().into_span().as_str();
-                        let b = u16::from_str_radix(operand, 16).unwrap();
                         let b0 = (0x00FF & b) as u8;
                         let b1 = (0xFF00 & b >> 8) as u8;
                         codes.push(b1);
@@ -256,30 +291,28 @@ fn main() {
 
                     },
                     Rule::ah => {
-                        codes.push(0xB0 + 0x04); 
-                        let operand = pairs.next().unwrap().clone().into_span().as_str();
-                        let b = u8::from_str_radix(operand, 16).unwrap();
-                        codes.push(b);
-                        current_address += 2;
+                      //  codes.push(0xB0 + 0x04); 
+                      //  let b = u8::from_str_radix(op2.as_str(), 16).unwrap();
+                      //  codes.push(b);
+                      //  current_address += 2;
                     },
                     Rule::bx => {  //16bit
-                        codes.push(0xB8 + 0x03); 
-                        let operand = pairs.next().unwrap().clone().into_span().as_str();
-                        let b = u16::from_str_radix(operand, 10).unwrap();
-                        let b0 = (0x00FF & b) as u8;
-                        let b1 = (0xFF00 & b >> 8) as u8;
-                        codes.push(b0);
-                        codes.push(b1);
-                        current_address += 3;
+                    //    codes.push(0xB8 + 0x03); 
+                    //    let b = u16::from_str_radix(op2.as_str(), 10).unwrap();
+                    //    let b0 = (0x00FF & b) as u8;
+                    //    let b1 = (0xFF00 & b >> 8) as u8;
+                    //    codes.push(b0);
+                    //    codes.push(b1);
+                    //    current_address += 3;
                     },
                     _ => unreachable!()
                 }
-            }
+            },
             _ => unreachable!()
         }
     }
-    //check: throw exception if there is a label to which no address is assigned.
-    for (label, &address) in labels_map.iter() {
+        //check: throw exception if there is a label to which no address is assigned.
+    for (_, &address) in labels_map.iter() {
         if address == 0 {
             panic!("Some label has no value.");
         }
