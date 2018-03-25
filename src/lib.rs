d00f7e72f6e17fa9e3a778ce9ed8a7822ea57d52
taba <hgKaguyaToupako4er@gmail.com>
2018-03-26 00:40:50 +0900

    Add harib01c

--- src/lib.rs
+++ src/lib.rs
@@ -7,51 +7,44 @@
 //void HariMain(void)
 //{
 //	int i; 
+//	int* p;
 //
-//	for (i = 0xa0000; i <= 0xaffff; i++) {
-//		write_mem8(i, i & 0x0f);
-//	}
+//for (i = 0xa0000; i <= 0xaffff; i++) {
+//    p = i;
+//    *p = i & 0x0f;
+//}
 //
 //	for (;;) {
 //	}
 //}
+
 #[no_mangle]
 pub extern fn rust_main() {
     for i in 0xa0000..0xb0000 {
-        write_mem8(i, i & 0x0f);
+        let p = i as *mut i32;
+        unsafe {
+            *p = i & 0x0f;
+        }
     }
-
-    //let hello = b"Hello World!";
-    //let color_byte = 0x07;//white  0x1f; // white foreground, blue background
-    //let mut hello_colored = [color_byte; 24];
-    //for (i, char_byte) in hello.into_iter().enumerate() {
-    //    hello_colored[i*2] = *char_byte;
-    //}
-
-    //// write `Hello World!` to the center of the VGA text buffer
-    //let buffer_ptr = (0xa0000 + 100 + 100) as *mut _;
-    ////let buffer_ptr = (0xa00000) as *mut _;
-    //unsafe { *buffer_ptr = hello_colored };
     loop{}
 }
-
 
 //_write_mem8:	; void write_mem8(int addr, int data);
 //		MOV		ECX,[ESP+4]		; [ESP+4]にaddrが入っているのでそれをECXに読み込む
 //		MOV		AL,[ESP+8]		; [ESP+8]にdataが入っているのでそれをALに読み込む
 //		MOV		[ECX],AL
 //		RET
-#[no_mangle]
-#[inline(never)]
-pub extern fn write_mem8(addr: u32, data: u32){
-    unsafe {
-        let ecx: u32;
-        let al:   u8;
-        asm!("movl $0, %ecx":"={ecx}"(ecx):"0"(addr)::"volatile");
-        asm!("movb $0, %al" :"={al}"(al):"0"(data)::"volatile");
-        asm!("movb %al, (%ecx)"::"%al"(al)::"volatile");
-    }
-}
+//#[no_mangle]
+//#[inline(never)]
+//pub extern fn write_mem8(addr: u32, data: u32){
+//    unsafe {
+//        let ecx: u32;
+//        let al:   u8;
+//        asm!("movl $0,   %ecx" :"={ecx}"(ecx):"0"(addr)::"volatile");
+//        asm!("movb $0,   %al"  :"={al}"(al):"0"(data)::"volatile");
+//        asm!("movb %al, (%ecx)"::"%al"(al)::"volatile");
+//    }
+//}
 
 
 #[lang = "eh_personality"] #[no_mangle] pub extern fn eh_personality() {}
