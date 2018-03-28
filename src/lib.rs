dd868276c0154ec8f27f6b27af9f145bd8a4842e
taba <hgKaguyaToupako4er@gmail.com>
2018-03-28 20:26:59 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -97,26 +97,8 @@ pub extern fn rust_main() {
 #[no_mangle]
 #[inline(never)]
 pub extern fn init_palette(){
-    static table_rgb: [u8; 16 * 3] = [
-        0x00, 0x00, 0x00,	/*  0:黒 */
-        0xff, 0x00, 0x00,	/*  1:明るい赤 */
-        0x00, 0xff, 0x00,	/*  2:明るい緑 */
-        0xff, 0xff, 0x00,	/*  3:明るい黄色 */
-        0x00, 0x00, 0xff,	/*  4:明るい青 */
-        0xff, 0x00, 0xff,	/*  5:明るい紫 */
-        0x00, 0xff, 0xff,	/*  6:明るい水色 */
-        0xff, 0xff, 0xff,	/*  7:白 */
-        0xc6, 0xc6, 0xc6,	/*  8:明るい灰色 */
-        0x84, 0x00, 0x00,	/*  9:暗い赤 */
-        0x00, 0x84, 0x00,	/* 10:暗い緑 */
-        0x84, 0x84, 0x00,	/* 11:暗い黄色 */
-        0x00, 0x00, 0x84,	/* 12:暗い青 */
-        0x84, 0x00, 0x84,	/* 13:暗い紫 */
-        0x00, 0x84, 0x84,	/* 14:暗い水色 */
-        0x84, 0x84, 0x84	/* 15:暗い灰色 */
-    ];
 
-    set_palette(0, 15, &table_rgb);
+    set_palette(0, 15);//, &table_rgb);
 
 }
 
@@ -137,20 +119,47 @@ pub extern fn init_palette(){
 //}
 #[no_mangle]
 #[inline(never)]
-pub extern fn set_palette(start: u8, end: u8, rgb: &[u8; 48])
+pub extern fn set_palette(start: u8, end: u8)//, rgb: &[u8; 48])
 {
+    const rgb: [u8; 16 * 3] = [
+        0x00, 0x00, 0x00,	/*  0:黒 */
+        0xff, 0x00, 0x00,	/*  1:明るい赤 */
+        0x00, 0xff, 0x00,	/*  2:明るい緑 */
+        0xff, 0xff, 0x00,	/*  3:明るい黄色 */
+        0x00, 0x00, 0xff,	/*  4:明るい青 */
+        0xff, 0x00, 0xff,	/*  5:明るい紫 */
+        0x00, 0xff, 0xff,	/*  6:明るい水色 */
+        0xff, 0xff, 0xff,	/*  7:白 */
+        0xc6, 0xc6, 0xc6,	/*  8:明るい灰色 */
+        0x84, 0x00, 0x00,	/*  9:暗い赤 */
+        0x00, 0x84, 0x00,	/* 10:暗い緑 */
+        0x84, 0x84, 0x00,	/* 11:暗い黄色 */
+        0x00, 0x00, 0x84,	/* 12:暗い青 */
+        0x84, 0x00, 0x84,	/* 13:暗い紫 */
+        0x00, 0x84, 0x84,	/* 14:暗い水色 */
+        0x84, 0x84, 0x84	/* 15:暗い灰色 */
+    ];
+
     let eflags = io_load_eflags();	/* 割り込み許可フラグの値を記録する */
     io_cli();                   	/* 許可フラグを0にして割り込み禁止にする */
     io_out8(0x03c8, start);
-    let p = rgb as *const u8;
-    for i in start..(end + 1) {
+    let p = &rgb as *const u8;
+    for i in 0..48 {
         unsafe {
-            io_out8(0x03c9, (*p + 3 * i) / 4);
-            io_out8(0x03c9, (*p + 3 * i + 1) / 4);
-            io_out8(0x03c9, (*p + 3 * i + 2) / 4);
+            io_out8(0x03c9,  *p.offset(i as isize) / 4);
         }
     }
-
+//    for i in start..(end + 1) {
+//       unsafe {
+//           io_out8(0x03c9,  *p.offset(3 as isize));
+//           io_out8(0x03c9,  *p.offset(4 as isize));
+//           io_out8(0x03c9,  *p.offset(5 as isize));
+//           //io_out8(0x03c9, *p.offset((3 * i    ) as isize) );
+//           //io_out8(0x03c9, *p.offset((3 * i + 1) as isize) );
+//           //io_out8(0x03c9, *p.offset((3 * i + 2) as isize) );
+//       }
+//    }
+//
 	io_store_eflags(eflags);	/* 割り込み許可フラグを元に戻す */
 }
 
@@ -215,7 +224,7 @@ pub extern fn io_out8(port: u32, data: u8){
     let al : u8;
     unsafe {
         asm!("movl $0,   %edx" :"={edx}"(edx):"0"(port)::"volatile");
-        asm!("movb $0,   %al"  :"={al}"(al):"0"(data)::"volatile");
+        asm!("movb $0,   %al"  :"={al}"(al)  :"0"(data)::"volatile");
         asm!("out  %al,  %dx");
         asm!("ret");
 
